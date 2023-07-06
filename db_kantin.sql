186cd27264bac9c4cf10155e15b4a1525420999a
Meylanza <meylan746@gmail.com>
2023-07-06 09:25:35 +0700

    innite commit

--- db_kantin.sql
+++ db_kantin.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jun 07, 2023 at 07:00 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Jul 2023 pada 04.17
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,28 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_admin`
+-- Struktur dari tabel `tb_admin`
 --
 
 CREATE TABLE `tb_admin` (
   `id` int(11) NOT NULL,
   `username` varchar(10) NOT NULL,
   `password` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_admin`
+-- Dumping data untuk tabel `tb_admin`
 --
 
 INSERT INTO `tb_admin` (`id`, `username`, `password`) VALUES
 (5, 'root', 'root123'),
 (6, 'root', 'root'),
-(7, 'test', 'root');
+(7, 'test', 'root'),
+(8, 'root', '123'),
+(9, 'admin1', 'admin234');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_makanan`
+-- Struktur dari tabel `tb_makanan`
 --
 
 CREATE TABLE `tb_makanan` (
@@ -53,44 +55,43 @@ CREATE TABLE `tb_makanan` (
   `nama_makanan` varchar(50) NOT NULL,
   `jenis_makanan` varchar(15) NOT NULL,
   `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_makanan`
+-- Dumping data untuk tabel `tb_makanan`
 --
 
 INSERT INTO `tb_makanan` (`id`, `nama_makanan`, `jenis_makanan`, `harga`) VALUES
-(6, 'KerangLaut', 'Makanan', 50000),
-(9, 'Kentang Goreng -test', 'Makanan -test', 100009);
+(6, 'KerangLaut', 'Makanan', 50000);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_admin`
+-- Indeks untuk tabel `tb_admin`
 --
 ALTER TABLE `tb_admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tb_makanan`
+-- Indeks untuk tabel `tb_makanan`
 --
 ALTER TABLE `tb_makanan`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tb_admin`
+-- AUTO_INCREMENT untuk tabel `tb_admin`
 --
 ALTER TABLE `tb_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `tb_makanan`
+-- AUTO_INCREMENT untuk tabel `tb_makanan`
 --
 ALTER TABLE `tb_makanan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
